fdcc7d6393c87b631fa1239ea2f9661c75fb0c13
rrprajiv <88769287+rrprajiv@users.noreply.github.com>
2021-11-10 22:24:59 +0530

    introduced getTodosForUser

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,16 +1,17 @@
 import 'source-map-support/register'
 import { createLogger } from '../../utils/logger'
+import { getTodosForUser } from '../../businessLogic/todos'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
-import  * as AWS  from 'aws-sdk'
 import { cors } from 'middy/middlewares'
 
 
 //import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
 import { getUserId } from '../utils';
 
-const docClient = new AWS.DynamoDB.DocumentClient()
+
+const logger  = createLogger('createTodo')
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
@@ -18,33 +19,16 @@ export const handler = middy(
     // Write your code here
     const userId = getUserId(event);
 
-    
-    const todosTable = process.env.TODOS_TABLE
-    const todosIndex = process.env.TODOS_CREATED_AT_INDEX
+    logger.log('info', `inside getTodos for user : ${userId}`)
+    const items = await getTodosForUser(userId)
 
-    const logger = createLogger('zzzz')
-    logger.log('info', 'Inside getTodos-Handler : TESTING LOGGER')
-
-    console.log(`Inside getTodos Handler : Table : ${todosTable} Index : ${todosIndex}`)
-    const todos = await docClient.query({
-      TableName : todosTable,
-      IndexName : todosIndex,
-      KeyConditionExpression: 'userId = :userId',
-      ExpressionAttributeValues: {
-          ':userId': userId
-      }
-    }).promise()
-
-    // const todos = '...'
-
-    console.log(`userid : ${userId} todosCount : ${todos.Count}`);
     return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*'
       },
       body: JSON.stringify({
-        "items": todos.Items
+        "items": items
       })
     }
 
